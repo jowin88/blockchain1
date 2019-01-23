2ea155a89f26c79a4fcbca284b82e4437e8d54a3
Jowin <jowin88@gmail.com>
2019-01-24 01:53:06 +0800

    blockchain part 1
    episode 3 - I have successfully done it

--- next_block.js
+++ next_block.js
@@ -5,6 +5,6 @@ exports.next_block = function(last_block)
 	this.timestamp = Date();
 	this.data = "Hey! I'm block " + String(this.index);
 	this.hash = last_block.hash;
-	block = new block(this.index, this.timestamp, this.data, this.hash);
-	return block;
+	the_block = new block(this.index, this.timestamp, this.data, this.hash);
+	return the_block;
 };
